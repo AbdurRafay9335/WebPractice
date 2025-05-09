7ef4393aa6218d1055fbc3cf7bae5705e8a642c2
AbdurRafay9335 <125581070+AbdurRafay9335@users.noreply.github.com>
2025-05-09 12:27:03 +0500

    push

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const app = express();
 const serverless = require('serverless-http');
 
+const app = express();
+
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.send('Hello from Express on Vercel!');
 });
 
 module.exports.handler = serverless(app);
